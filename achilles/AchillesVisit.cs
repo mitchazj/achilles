99935563043f6778d4fa2c5e790a103c6566f21a
Mitchell Johnson <mitchell@cardlify.com>
2019-05-19 16:45:40 +1000

    Initial Commit

---
+++ achilles/AchillesVisit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+
+namespace achilles {
+    public class AchillesVisit {
+        public Uri Target { get; private set; }
+        public DateTime Time { get; private set; }
+        public CookieCollection Cookies { get; private set; }
+        public AchillesVisit(Uri target, DateTime time, CookieCollection cookies) {
+            Target = target; Time = time; Cookies = cookies;
+        }
+    }
+
+    public class AchillesHistory : List<AchillesVisit> {}
+}
